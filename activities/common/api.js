df4e1d2b0b986494c03956b1fa75a2bad33d8471
olliejm <olliejm@protonmail.com>
2019-05-14 15:09:04 +0100

    reflect latest output of yo

--- activities/common/api.js
+++ activities/common/api.js
@@ -4,19 +4,27 @@ const got = require('got');
 const HttpAgent = require('agentkeepalive');
 const HttpsAgent = HttpAgent.HttpsAgent;
 
+let _activity = null;
+
 function api(path, opts) {
   if (typeof path !== 'string') {
     return Promise.reject(new TypeError(`Expected \`path\` to be a string, got ${typeof path}`));
   }
 
+  let agent = {
+    http: new HttpAgent(),
+    https: new HttpsAgent()
+  };
+
+  if (_activity.Context.ProxyServer && _activity.Context.ProxyServer.agent) {
+    agent = _activity.Context.ProxyServer.agent;
+  }
+
   opts = Object.assign({
     json: true,
-    token: Activity.Context.connector.token,
-    endpoint: Activity.Context.connector.endpoint,
-    agent: {
-      http: new HttpAgent(),
-      https: new HttpsAgent()
-    }
+    token: _activity.Context.connector.token,
+    endpoint: _activity.Context.connector.endpoint,
+    agent: agent
   }, opts);
 
   opts.headers = Object.assign({
@@ -24,15 +32,11 @@ function api(path, opts) {
     'user-agent': 'adenin Digital Assistant, https://www.adenin.com/digital-assistant/'
   }, opts.headers);
 
-  if (opts.token) {
-    opts.headers.Authorization = `Bearer ${opts.token}`;
-  }
+  if (opts.token) opts.headers.Authorization = `Bearer ${opts.token}`;
 
   const url = /^http(s)\:\/\/?/.test(path) ? path : opts.endpoint + path;
 
-  if (opts.stream) {
-    return got.stream(url, opts);
-  }
+  if (opts.stream) return got.stream(url, opts);
 
   return got(url, opts).catch((err) => {
     throw err;
@@ -48,6 +52,10 @@ const helpers = [
   'delete'
 ];
 
+api.initialize = (activity) => {
+  _activity = activity;
+};
+
 api.stream = (url, opts) => got(url, Object.assign({}, opts, {
   json: false,
   stream: true
@@ -58,21 +66,5 @@ for (const x of helpers) {
   api[x] = (url, opts) => api(url, Object.assign({}, opts, {method}));
   api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, {method}));
 }
-
-api.handleError = function (error, authRequiresStatusCodes) {
-  return Activity.handleError(error, authRequiresStatusCodes);
-};
-
-api.isErrorResponse = function (response, successStatusCodes) {
-  return Activity.isErrorResponse(response, successStatusCodes);
-};
-
-api.dateRange = function (defaultRange) {
-  return Activity.dateRange(defaultRange);
-};
-
-api.pagination = function () {
-  return Activity.pagination();
-};
 
 module.exports = api;
